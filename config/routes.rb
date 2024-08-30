e060d503574d86fcc15f100c6572dc2ec92cf374
ootukaai <dodogamine417.9@gmail.com>
2024-08-30 12:10:30 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :show, :create]
+  resources :items, only: [:index, :new, :show, :create, :destroy]
 
  
 end
