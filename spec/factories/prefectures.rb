3dec4af89de8613c8b6d0f7e99c2944721b0f0a4
ootukaai <dodogamine417.9@gmail.com>
2024-09-10 19:08:26 +0900

    コードレビュー修正後４

--- spec/factories/prefectures.rb
+++ spec/factories/prefectures.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     first_address_id { 2 }
     second_address { "京都市" }
     third_address { "山科区" }
-    forth_address { "" }
+    forth_address { "京都マンション" }
     post_num { "600-3333" }
     tel_num { "60033333333" }
     token {"tok_abcdefghijk00000000000000000"}
