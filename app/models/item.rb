55e4cf2c141ddf035c86a2ce69ceeef963d6abb2
ootukaai <dodogamine417.9@gmail.com>
2024-09-11 14:59:08 +0900

    本番環境修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :feature, presence: true
   validates :price, presence: true,numericality: {only_integer:true,less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
   validates :image, presence: true
-  validates :first_address_id,  presence: true,numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :address_id,  presence: true,numericality: { other_than: 1 , message: "can't be blank" } 
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :status_id, presence: true,numericality: { other_than: 1, message: "can't be blank"} 
   validates :shipping_day_id, presence: true,numericality: { other_than: 1 , message: "can't be blank" } 
